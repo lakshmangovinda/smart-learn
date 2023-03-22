265270b00599b85873e8c182379fb3ce75bfaff2
lakshmangovinda <lakshmangovind1998@gmail.com>
2023-03-22 12:42:41 +0530

    fixed changed font in NAV bar

--- src/Components/navigation/styledNav.js
+++ src/Components/navigation/styledNav.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const HomePageTitle = styled.div`
   a {
     color: white !important;
-    font-size: 40px ;
-    font-family: system-ui ;
+    font-size: 40px;
+    font-family: system-ui;
     font-weight: 600;
     text-decoration: none !important;
-    padding: 2rem ;
+    padding: 2rem;
     cursor: pointer !important;
   }
 `;
